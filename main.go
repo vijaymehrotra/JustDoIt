fc870bf4d02f0e99de1b278849a6eb1aa46a78b6
vijaymehrotra <vijaymehrotra99@gmail.com>
2024-08-01 11:55:08 +0530

    App Name is Changed

--- main.go
+++ main.go
@@ -54,10 +54,10 @@ func main() {
 
 	app := fiber.New()
 
-	// app.Use(cors.New(cors.Config{
-	// 	AllowOrigins: "http://localhost:5173",
-	// 	AllowHeaders: "Origin,Content-Type,Accept",
-	// }))
+	//app.Use(cors.New(cors.Config{
+	//	AllowOrigins: "http://localhost:5173",
+	//	AllowHeaders: "Origin,Content-Type,Accept",
+	//}))
 
 	app.Get("/api/todos", getTodos)
 	app.Post("/api/todos", createTodo)
